e10267bc6cb0efb3f0aba4ec98c47e4b6169468f
Yashas <yashaspancham@gmail.com>
2025-07-30 13:34:51 +0530

    Yashas-feat-test

---
+++ test_function.py
@@ -0,0 +1,31 @@
+import json
+import lambda_function
+from moto import mock_dynamodb
+import boto3
+import os
+
+@mock_dynamodb
+def test_lambda_handler():
+    # Setup mocked DynamoDB
+    os.environ["TABLE_NAME"] = "TestTable"
+    os.environ["PARTITION_KEY"] = "counter"
+    os.environ["PARTITION_VALUE"] = "visits"
+
+    client = boto3.client('dynamodb', region_name='us-east-1')
+    client.create_table(
+        TableName="TestTable",
+        KeySchema=[{"AttributeName": "counter", "KeyType": "HASH"}],
+        AttributeDefinitions=[{"AttributeName": "counter", "AttributeType": "S"}],
+        BillingMode="PAY_PER_REQUEST"
+    )
+    client.put_item(
+        TableName="TestTable",
+        Item={"counter": {"S": "visits"}, "count": {"N": "0"}}
+    )
+
+    response = lambda_function.lambda_handler({}, {})
+    body = json.loads(response["body"])
+
+    assert response["statusCode"] == 200
+    assert "visitorsCount" in body
+    assert isinstance(body["visitorsCount"], int)
