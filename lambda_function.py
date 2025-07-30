5f0886952858c24b4f64c7a5e7dfb0d1ab87b8c4
Yashas <yashaspancham@gmail.com>
2025-07-30 09:39:36 +0530

    Yashas-feat-firstCommit

---
+++ lambda_function.py
@@ -0,0 +1,37 @@
+import json
+import boto3
+import os
+
+dynamodb = boto3.client('dynamodb')
+
+TABLE_NAME = os.getenv('TABLE_NAME')
+PARTITION_KEY = os.getenv('PARTITION_KEY')
+PARTITION_VALUE = os.getenv('PARTITION_VALUE')
+
+def lambda_handler(event, context):
+    response = dynamodb.update_item(
+        TableName=TABLE_NAME,
+        Key={
+            PARTITION_KEY: {'S': PARTITION_VALUE}
+        },
+        UpdateExpression='ADD #count :incr',
+        ExpressionAttributeNames={
+            '#count': 'count'
+        },
+        ExpressionAttributeValues={
+            ':incr': {'N': '1'}
+        },
+        ReturnValues='UPDATED_NEW'
+    )
+
+    new_count = int(response['Attributes']['count']['N'])
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,PUT"
+        },
+        "body": json.dumps({"Visitcount": new_count})
+    }
